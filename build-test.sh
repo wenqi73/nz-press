70ed2f4a5e12dfaffb8f835c38598298ff29c0ea
Wenqi <1264578441@qq.com>
2018-11-11 21:24:51 +0800

    fix(package): bin path and ci (#17)
    # fix bin path to bin/nzpress
    
    # add error process when calling cli

--- build-test.sh
+++ build-test.sh
@@ -3,10 +3,10 @@
 set -u -e -o pipefail
 
 # document has config and languages
-node --max-old-space-size=3000 `pwd`/bin/nzpress build ./docs --base-href=./
+nzpress build ./docs --base-href=./
 
 # simple document
 mkdir docs1
 echo "# Hello NzPress" > docs1/README.md
-node `pwd`/bin/nzpress build ./docs1
+nzpress build ./docs1
 rm -rf ./docs1
