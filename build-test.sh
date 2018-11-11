dbe1b9a75db2ac27d50c6b7441958f4e1b7c65b3
wenqi73 <1264578441@qq.com>
2018-11-11 15:25:00 +0800

    chore: change test

--- build-test.sh
+++ build-test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 readonly dir=`pwd`
-cd docs
-bash ${dir}/bin/nzpress.sh build
+node ${dir}/bin/nzpress build ./docs --base-href=./
 cd ..
 mkdir docs1 && cd docs1
 echo '# Hello NzPress' > README.md
-bash ${dir}/bin/nzpress.sh build
+node ${dir}/bin/nzpress build ./
--- scripts/template/app-routing.template.ts
+++ scripts/template/app-routing.template.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './layout.component';
+import { LayoutComponent } from './share/layout.component';
 
 {{imports}}
 
