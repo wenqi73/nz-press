a3be0bc2d4da4526eaf3ab9e92a6eba56b3260f1
wenqi73 <1264578441@qq.com>
2018-11-11 20:30:39 +0800

    fix(package): bin path

--- build-test.sh
+++ build-test.sh
@@ -2,11 +2,12 @@
 
 set -u -e -o pipefail
 
+nzpress -v
 # document has config and languages
-node --max-old-space-size=3000 `pwd`/bin/nzpress build ./docs --base-href=./
+nzpress build ./docs --base-href=./
 
 # simple document
 mkdir docs1
 echo "# Hello NzPress" > docs1/README.md
-node `pwd`/bin/nzpress build ./docs1
+nzpress build ./docs1
 rm -rf ./docs1
