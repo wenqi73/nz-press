c832581b4b9d45a7307b34229a07772b57634866
wenqi73 <1264578441@qq.com>
2018-11-16 00:25:32 +0800

    fix: build ci

--- build-test.sh
+++ build-test.sh
@@ -3,11 +3,10 @@
 set -u -e -o pipefail
 
 # document has config and languages
-`pwd`/bin/nzpress build ./docs
-`pwd`/bin/nzpress build ./2.x-doc --base-href=./
+nzpress build ./docs --base-href=./
 
 # simple document
 mkdir docs1
 echo "# Hello NzPress" > docs1/README.md
-`pwd`/bin/nzpress build ./docs1
+nzpress build ./docs1
 rm -rf ./docs1
